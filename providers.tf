88950e548ff239283d7bff0d1788a08aae0b842f
Raed Mansour <raied272@gmail.com>
2025-12-14 21:44:11 +0200

    Added providers,

---
+++ providers.tf
@@ -0,0 +1,9 @@
+provider "google" {
+  project = "raed-test-project-480418"
+  region  = "europe-west1"
+}
+
+provider "google-beta" {
+  project = "raed-test-project-480418"
+  region  = "europe-west1"
+}
